05b07689da9fca268710a622679788848a02c34e
Barthandelous01 <calvarium2009@hotmail.com>
2020-03-02 13:00:42 -0500

    added CLI args

--- src/redocx.c
+++ src/redocx.c
@@ -2,9 +2,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-     get_contents("/Users/calebbrzezinski/Desktop/wc3.docx");
+     /* init some variables */
+     char *filename = safe_malloc(200);
+     char *txtname = safe_malloc(200);
+     int opt;
+
+     /* parse cli args */
+     while((opt = getopt(argc, argv, ":f:")) != -1)
+     {
+          switch(opt)
+          {
+          case 'f':
+               filename = optarg;
+               printf("filename: %s\n", optarg);
+               break;
+          case 'o':
+               txtname = optarg;
+               printf("output file: %s\n", optarg);
+               break;
+          case ':':
+               printf("option needs a value\n");
+               break;
+          case '?':
+               printf("unknown option: %c\n", optopt);
+          break;
+          }
+     }
+     /* write contents of file to temp.xml */
+     get_contents(filename);
+
+     /* return */
      return 0;
 }
