c5c4c6e289fb9f47e9bcc0df7ba9060e0d90a617
Barthandelous01 <cbrzezinski@wccnet.edu>
2020-09-13 12:17:44 -0400

    Added longopts support

--- src/redocx.c
+++ src/redocx.c
@@ -6,11 +6,32 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <unistd.h>
+#include <config.h>
 
 void usage()
 {
-     printf("%s", "usage: redocx -f file [-x] [-o output]");
+     printf("%s", "A blazing-fast .docx decoder\n"
+            "\n"
+            "USAGE:\n"
+            "    redocx -f file [-o outfile] [-x]\n"
+            "\n"
+            "OPTIONS:\n"
+            "-V, --version           Print version info and exit\n"
+            "-h, --help              Print tis message and exit\n"
+            "-x, --xml               Leave temporary XML file\n"
+            "-f, --filename          The filename of the docx file\n"
+            "-o, --outfile           The output text file\n");
+     exit(0);
+}
+
+void version()
+{
+     printf("%s", PACKAGE_STRING "\nCopyright (C) 2020 Barthandelous01\n\
+License RBSD 3-Clause License.\n\
+This is free software; you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n");
      exit(0);
 }
 
@@ -25,7 +46,14 @@ int main(int argc, char *argv[])
      int xmlarg = 0;
 
      /* parse cli args */
-     while((opt = getopt(argc, argv, "f:ho:x")) != -1)
+     static struct option longopts[] = {
+         {"filename", required_argument, NULL, 'f'},
+         {"outfile",  required_argument, NULL, 'o'},
+         {"xml",      no_argument,       NULL, 'x'},
+         {"help",     no_argument,       NULL, 'h'},
+         {"version",  no_argument,       NULL, 'V'}
+     };
+     while((opt = getopt_long(argc, argv, "f:ho:xV", longopts, NULL)) != -1)
      {
           switch(opt)
           {
@@ -40,6 +68,9 @@ int main(int argc, char *argv[])
                break;
           case 'x':
                xmlarg = 1;
+               break;
+          case 'V':
+               version();
                break;
           case ':':
                printf("option needs a value\n");
