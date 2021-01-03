779fbc6f9bad09806c2afae6bed7776b934c4a3b
Caleb D.S. Brzezinski <calebdsb@protonmail.com>
2021-01-03 16:37:34 -0500

    Happy New Year~

--- src/redocx.c
+++ src/redocx.c
@@ -28,7 +28,7 @@ void usage()
 
 void version()
 {
-     printf("%s", PACKAGE_STRING "\nCopyright (C) 2020 Barthandelous01\n\
+     printf("%s", PACKAGE_STRING "\nCopyright (C) 2020-2021 Barthandelous01\n\
 License RBSD 3-Clause License.\n\
 This is free software; you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n");
